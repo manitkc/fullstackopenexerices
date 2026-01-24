4b0e974c473669c8f3d2cb1fd2501594dc3425ab
Manit Kc <mkc184@aucklanduni.ac.nz>
2026-01-24 14:24:29 +1300

    completed exercise 1.4

--- Part1Exercises/courseinfo/src/App.jsx
+++ Part1Exercises/courseinfo/src/App.jsx
@@ -1,21 +1,65 @@
 const App = () => {
+  console.log("hi")
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      ...
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
+const Header = (prop) => {
+  return (
+    <>
+    <h1>
+      {prop.course}
+    </h1>
+    
+    </>
+  )
+
+}
+const Content = (prop) => {
+  const content = prop.parts.map((value) => 
+    <li>
+      {value.name}-{value.exercises}
+    </li>
+  )
+  return (
+    <>
+    <ul>
+      {content}
+    </ul>
+    </>
+  )
+
+}
+const Total = (prop) => {
+  const total = prop.parts[0].exercises +prop.parts[1].exercises +prop.parts[2].exercises
+  return (
+    <>
+    <p>
+      Total = {prop.parts[0].exercises} + {prop.parts[1].exercises}+ {prop.parts[2].exercises} = {total}
+    </p>
+    </>
+  )
+
+}
+
+export default App
