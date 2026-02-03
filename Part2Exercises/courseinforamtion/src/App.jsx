e3ef2dc9e9e5541b27389f158f2d18a2f5de2fdc
Manit Kc <mkc184@aucklanduni.ac.nz>
2026-02-03 22:44:00 +1300

    completed exercise 2.1

---
+++ Part2Exercises/courseinforamtion/src/App.jsx
@@ -0,0 +1,58 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <Course course = {course}/>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <>
+      <Header name = {course.name}/>
+      <Content content = {course.parts}/>
+    </>
+  )
+}
+
+const Header = ({name}) => <h1>{[name]}</h1>
+
+const Content = ({content}) => {
+  console.log (content)
+  return (
+    <>
+    {content.map((part) => 
+      <Part key = {part.id}  name = {part.name} exercisesNo = {part.exercises}/>
+    )}
+    </>
+  )
+}
+const Part = ({name ,exercisesNo }) => {
+  console.log("hi from part ")
+  return (
+    <div>{name} {exercisesNo}</div>
+  )
+}
+
+
+export default App
